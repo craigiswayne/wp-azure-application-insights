dbe55f4225e48dfa411f1136913805f1c12bf774
Craig Wayne <craigiswayne@gmail.com>
2024-02-23 11:18:06 +0000

    Validation for connection string

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-Javascript snippet taken from:
+# Azure Application Insights for WordPress
 
+Javascript snippet taken from:
 https://github.com/microsoft/ApplicationInsights-JS
+
+---
+
+### TODO:
+[ ] Composer package
+[ ] php linting
--- wp-azure-application-insights.php
+++ wp-azure-application-insights.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Azure Application Insights
+Plugin Name: Azure App Insights
 Description: Enable Azure Application Insights for your website
 Plugin URI: https://github.com/craigiswayne/wp-azure-application-insights
 Version: 3.2.4
@@ -11,11 +11,14 @@ Requires PHP: 8.2
 License:  MIT
 */
 
+const WP_AZURE_APPLICATION_INSIGHTS_PREFIX = 'az_app_insights_';
+
 class AzureApplicationInsights {
-    public static string $page_id = 'az_app_insights_my-plugin-settings';
-    public static string $section_id = 'az_app_insights_my_plugin_section';
-    public static string $option_group = 'az_app_insights_options';
-	public static string $option_name = 'az_app_insights_options_connection_string';
+    public static string $page_id = WP_AZURE_APPLICATION_INSIGHTS_PREFIX.'my-plugin-settings';
+    public static string $section_id = WP_AZURE_APPLICATION_INSIGHTS_PREFIX.'my_plugin_section';
+    public static string $option_group = WP_AZURE_APPLICATION_INSIGHTS_PREFIX.'options';
+	public static string $option_name = WP_AZURE_APPLICATION_INSIGHTS_PREFIX.'options_connection_string';
+    public static string $regex_connection_string = 'InstrumentationKey=[a-z\d]{8}-[a-z\d]{4}-[a-z\d]{4}-[a-z\d]{4}-[a-z\d]{12};IngestionEndpoint=https:\/\/.*;LiveEndpoint=https:\/\/';
 
 
 	public static function init(): void {
@@ -27,8 +30,8 @@ class AzureApplicationInsights {
 
 	public static function create_menu_item(): void {
 		add_plugins_page(
-			'Azure Application Insights',
-			'Azure App Insights',
+			get_plugin_data(__FILE__)['Name'],
+			get_plugin_data(__FILE__)['Name'],
 			'manage_options',
 			'azure-app-insights',
 			[__CLASS__, 'options_page_content']
@@ -42,6 +45,7 @@ class AzureApplicationInsights {
 			<h2><?= $plugin_data['Name']; ?></h2>
 			<form method="post" action="options.php">
 				<?php
+                    settings_errors(self::$option_group);
                     settings_fields(self::$option_group);
                     do_settings_sections(self::$page_id);
                     submit_button();
@@ -51,18 +55,25 @@ class AzureApplicationInsights {
 		<?php
 	}
 
-	public static function sanitize_option($value): string {
-		return sanitize_text_field($value);
-	}
-
 	public static function delete_options($network_deactivating): void {
 		delete_option(self::$option_name);
 	}
 
 	public static function admin_init(): void {
-		register_setting(self::$option_group, self::$option_name, [__CLASS__, 'sanitize_option']);
+		register_setting(self::$option_group, self::$option_name, [__CLASS__, 'sanitize_connection_string']);
 		add_settings_section(self::$section_id, 'Settings', [__CLASS__,'section_callback'], self::$page_id);
-		add_settings_field('az_app_insights_connection_string', 'Connection String', [__CLASS__, 'connection_string_field_callback'], self::$page_id, self::$section_id);
+		add_settings_field(self::$option_name, 'Connection String', [__CLASS__, 'connection_string_field_callback'], self::$page_id, self::$section_id);
+	}
+
+	public static function sanitize_connection_string($new_value): string {
+
+        if (preg_match('/'.self::$regex_connection_string.'/', $new_value)) {
+	        return sanitize_text_field($new_value);
+        }
+
+		$original_value = get_option(self::$option_name);
+		add_settings_error(self::$option_group, self::$option_name.'_regex_failed', 'Connection string: Incorrect format... reverting to original value', 'error');
+		return $original_value;
 	}
 
 
@@ -72,7 +83,8 @@ class AzureApplicationInsights {
 
 	public static function connection_string_field_callback(): void {
 		$value = get_option(self::$option_name);
-		echo '<input class="widefat" type="text" name="'.self::$option_name.'" value="'.esc_attr($value).'" />';
+		echo '<input class="widefat" type="text" required pattern="'.self::$regex_connection_string.'" name="'.self::$option_name.'" value="'.esc_attr($value).'" aria-describedby="'.self::$option_name.'_help" autocomplete="false"/>';
+        echo '<p class="description" id="'.self::$option_name.'_help">Format: <code>'.self::$regex_connection_string.'</code></p>';
 	}
 
     public static function add_js_snippet(): void {
@@ -92,4 +104,4 @@ class AzureApplicationInsights {
 AzureApplicationInsights::init();
 
 // TODO: register shutdown hook
-// TODO: validate the value of the connection string
+// TODO: dashboard widget for the app insights api

278076a39dd556e5e6b5a4a08d8977d590e708d5
craigiswayne <craigiswayne@users.noreply.github.com>
2024-03-25 15:13:03 +0000

    Incrementing version number

--- class-wp-azure-application-insights.php
+++ class-wp-azure-application-insights.php
@@ -3,7 +3,7 @@
  * Plugin Name: Azure App Insights
  * Description: "PLUGIN_DESCRIPTION"
  * Plugin URI: https://github.com/craigiswayne/wp-azure-application-insights
- * Version: 3.3.6-pre.ea07c74
+ * Version: 3.3.6
  * Author: Craig Wayne
  * Author URI: https://github.com/craigiswayne/
  * Requires at least: 6.4.2
